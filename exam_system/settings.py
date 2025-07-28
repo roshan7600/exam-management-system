8d2f7d171f3eb8f66e27b6d1ce24d530a46553ae
roshan7600 <roshanpatel31296@gamil.com>
2025-07-28 14:58:10 +0530

    changes for settings

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -9,17 +9,16 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-your-secret-key-here'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+
 DEBUG = False
 
 
-# ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['exam-management-system.onrender.com', 'localhost']
+
 ALLOWED_HOSTS = ['exam-management-system.onrender.com', 'localhost', '127.0.0.1']
 
 
--- exam_system/urls.py
+++ exam_system/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+# from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('exam_app.urls')),
+    # path('', views.home, name='home'),
 ]
 
 if settings.DEBUG:
