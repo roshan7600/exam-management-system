4ecf1dbf76a667210fdc5e0c6a764e00445d6ef5
roshan7600 <roshanpatel31296@gamil.com>
2025-07-28 16:00:59 +0530

    settings are update

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Installed apps
 INSTALLED_APPS = [
