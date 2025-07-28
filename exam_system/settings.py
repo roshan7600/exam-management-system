4fcb6ecafd0df8ec3b1b901e264413354549c825
roshan7600 <roshanpatel31296@gamil.com>
2025-07-28 16:22:53 +0530

    Final working version with proper .env and .gitignore

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Installed apps
